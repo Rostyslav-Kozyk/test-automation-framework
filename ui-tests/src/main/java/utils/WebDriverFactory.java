94f5b34f69c37b83c12f36cfc148df62dd9ae00c
Rostyslav-Kozyk <rostyk.niko3000@gmail.com>
2026-01-07 17:48:47 +0100

    feat: add WebDriverFactory for browser initialization

---
+++ ui-tests/src/main/java/utils/WebDriverFactory.java
@@ -0,0 +1,43 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+
+public class WebDriverFactory {
+
+    public static WebDriver createDriver(String browser, boolean headless) {
+        if (browser.equalsIgnoreCase("firefox")) {
+            return createFirefoxDriver(headless);
+        }
+        return createChromeDriver(headless);
+    }
+
+    private static WebDriver createChromeDriver(boolean headless) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+
+        if (headless) {
+            options.addArguments("--headless=new");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        return new ChromeDriver(options);
+    }
+
+    private static WebDriver createFirefoxDriver(boolean headless) {
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions options = new FirefoxOptions();
+
+        if (headless) {
+            options.addArguments("-headless");
+            options.addArguments("--width=1920");
+            options.addArguments("--height=1080");
+        }
+
+        return new FirefoxDriver(options);
+    }
+}
