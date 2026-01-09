9f5945163f97f406a57aa94ac01eb44eb618586b
Rostyslav-Kozyk <rostyk.niko3000@gmail.com>
2026-01-09 22:54:14 +0100

    feat: implement LoginPage using Page Object Model

---
+++ ui-tests/src/main/java/pages/LoginPage.java
@@ -0,0 +1,46 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+    private final WebDriver driver;
+
+    private static final String LOGIN_URL = "https://www.saucedemo.com/";
+
+    private final By usernameInput = By.id("user-name");
+    private final By passwordInput = By.id("password");
+    private final By loginButton = By.id("login-button");
+    private final By errorMessage = By.xpath("//h3[@data-test='error']");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void open() {
+        driver.get(LOGIN_URL);
+    }
+
+    public void enterUsername(String username) {
+        driver.findElement(usernameInput).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordInput).sendKeys(password);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
+
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
+
+    public String getErrorMessage() {
+        return driver.findElement(errorMessage).getText();
+    }
+}
